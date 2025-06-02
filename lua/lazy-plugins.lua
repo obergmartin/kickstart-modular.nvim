aa4fa0dee6ad24d3cafdf0bbb3d52f3835fdb9cc
Martin <obergma@gmail.com>
2025-06-01 20:57:07 -0700

    update for iron?

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -49,8 +49,6 @@ require('lazy').setup({
 
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for kickstart
   require 'custom/plugins/obsidian',
-  { 'Vigemus/iron.nvim' },
-  -- require 'custom/plugins/iron',
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
@@ -65,7 +63,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
