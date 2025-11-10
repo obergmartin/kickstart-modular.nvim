7b14ba4de4a0450171192b3416548691b35c0f86
Martin Oberg <obergma@gmail.com>
2025-11-09 21:27:53 -0800

    add vim-slime

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,18 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'jpalardy/vim-slime',
+    init = function()
+      vim.g.slime_no_mappings = 1
+      vim.g.slime_target = 'neovim'
+    end,
+    config = function()
+      vim.g.slime_cell_delimiter = '#%%'
+      --    vim.keymap.set("x", "gz", "<Plug>SlimeRegionSend", { remap = true, silent = false })
+      vim.keymap.set('n', '<leader>tc', '<Plug>SlimeSendCell', { desc = 'SlimeSendCell' })
+      vim.keymap.set('n', '<leader>tp', 'vip:SlimeSend<cr>', { desc = 'Slime Send Paragraph' })
+    end,
+  },
+}
