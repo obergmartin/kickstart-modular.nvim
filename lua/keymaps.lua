bcc57b90ab763cdb7a7edf846af9e67048ff5f52
Martin Oberg <obergma@gmail.com>
2026-01-28 20:31:54 -0800

    insert code block separator

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,6 +11,7 @@ local function ipy_run()
   vim.cmd('SlimeSend1' .. cmd_str)
 end
 vim.keymap.set('n', '<leader>lr', ipy_run, { desc = 'plt.show()' })
+vim.keymap.set('n', '<leader>lC', 'i# %%<c-[>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('n', 'tt', ':tab split<cr>')
